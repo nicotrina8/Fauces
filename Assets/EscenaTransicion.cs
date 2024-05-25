2b52759c5283afe63d52917802a8517b24c33a4e
jeffgarciahdez <161370919+jeffgarciahdez@users.noreply.github.com>
2024-05-25 22:32:12 +0100

    Añadir escenas
    Añadí fundidos a negro y la boca

---
+++ Assets/EscenaTransicion.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EscenaTransicion : MonoBehaviour
+{
+    private Animator animator;
+    [SerializeField] private AnimationClip animacionFInal;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            StartCoroutine(CambiarEscena());
+        }
+    }
+
+    IEnumerator CambiarEscena()
+    {
+        animator.SetTrigger("Iniciar");
+
+       yield return new WaitForSeconds(animacionFInal.length);
+
+       SceneManager.LoadScene(1); 
+    }
+
+}
