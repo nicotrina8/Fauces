ec25e83288fc8e2b4fe3fd6d0902dd936151867b
jeffgarciahdez <161370919+jeffgarciahdez@users.noreply.github.com>
2024-05-25 18:38:29 +0100

    Menu pausa
    Faltan las opciones socio

---
+++ Assets/Scripts/PipoScripts/MenuPausa.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuPausa : MonoBehaviour
+{
+    [SerializeField] private GameObject pauseMenuUI;
+
+    [SerializeField] private bool isPaused;
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            isPaused = !isPaused;
+        }
+        if(isPaused)
+        {
+            ActivateMenu();
+        }
+        else
+        {
+            DeactivateMenu();
+        }
+    }
+
+    void ActivateMenu()
+    {
+        Time.timeScale = 0;
+        pauseMenuUI.SetActive(true);
+    }
+    public void DeactivateMenu()
+    {
+        Time.timeScale = 1;
+        pauseMenuUI.SetActive(false);
+        isPaused = false;
+    }
+}
